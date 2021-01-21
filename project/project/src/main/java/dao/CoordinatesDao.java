49ff0e2125eea8ce37faaeac1cd9fd298e9bd6d3
admitsenc <56822389+admitsenc@users.noreply.github.com>
2021-01-21 18:33:30 +0200

    Create CoordinatesDao.java

---
+++ project/project/src/main/java/dao/CoordinatesDao.java
@@ -0,0 +1,11 @@
+package project;
+
+import java.util.List;
+
+public interface CoordinatesDao {
+	public List<Coordinates> getCoordinatesList(int WorkoutId);
+	public void saveCoordinate(float latitude, float longitude, Workout workout);
+	public void saveCoordinateList(List<Coordinates> coord) throws ApplicationException;
+	public void deleteCoordinates(int coordinateId);
+
+}
