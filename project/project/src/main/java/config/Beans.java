1eca8df37c1d270665b33ce35099e356eeaf465a
admitsenc <56822389+admitsenc@users.noreply.github.com>
2021-01-21 18:32:09 +0200

    Create Beans.java

---
+++ project/project/src/main/java/config/Beans.java
@@ -0,0 +1,26 @@
+package project;
+
+@Configuration
+public class Beans {
+  @Bean
+  public NodeService nodeService(final NodeRepository nodeRepository) {
+    return new NodeServiceImpl(nodeRepository);
+  }
+
+  @Bean
+  public DeviceService deviceService(final DeviceRepository deviceRepository,
+      final NodeService nodeService) {
+    return new DeviceServiceImpl(deviceRepository, nodeService);
+  }
+
+  @Bean
+  public CommandService commandService(final CommandRepository commandRepository,
+      final DeviceService deviceService) {
+    return new CommandServiceImpl(deviceService, commandRepository);
+  }
+
+  @Bean
+  public StateService stateService(final StateRepository stateRepository,
+      final CommandService commandService) {
+    return new StateServiceImpl(stateRepository, commandService);
+  }}
